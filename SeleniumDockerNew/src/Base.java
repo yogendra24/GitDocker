7d811dfa01e1ff88b2be361f712542c3bae2f8ab
yogendra24 <yogendra.londhe@gmail.com>
2021-09-01 18:16:53 +0530

    Update Base.java
    some change in base

--- SeleniumDockerNew/src/Base.java
+++ SeleniumDockerNew/src/Base.java
@@ -8,12 +8,12 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 
 import org.openqa.selenium.remote.RemoteWebDriver;
 
- 
+
 public class Base {
     protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
     public static String remote_url_chrome = "http://localhost:4445/wd/hub";
     public static String remote_url_firefox = "http://localhost:4446/wd/hub";
- 
+
     public static void setupThread(String browserName) throws MalformedURLException
     {
         if(browserName.equalsIgnoreCase("chrome"))
@@ -24,17 +24,17 @@ public class Base {
         }
         else if (browserName.equalsIgnoreCase("firefox"))
         {
-            System.out.println("Inside Firefox");
+            System.out.println("Inside Firefox driver");
             FirefoxOptions options = new FirefoxOptions();
             driver.set(new RemoteWebDriver(new URL(remote_url_firefox), options));
         }
     }
- 
+
     public static WebDriver getDriver()
     {
         return driver.get();
     }
- 
+
     public static void tearDownDriver()
     {
         getDriver().quit();
